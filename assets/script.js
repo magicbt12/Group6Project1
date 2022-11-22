7839052d20f15859c74477dac2248679a5664e8c
Melany Baldeon <tochio12910@gmail.com>
2022-11-21 22:00:02 -0500

    adding the api and key

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
+var apiKey = "08f00304851866898e77f5deff0d7eba";
+var apiUrl = "https://api.themoviedb.org/3"
+var apiSearch = apiUrl + "/search/movie"
+var testQuery = "Bourne";
 
-fetch("http://www.omdbapi.com/?apikey=[yourkey]&", {
-    method: "get"
-})
-.then(x=> x.)
+fetch(apiSearch + "?api_key=" + apiKey + "&query=" + testQuery)
