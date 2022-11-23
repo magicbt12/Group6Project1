2de4c85369dca13b3fe95393a93b5048060c68b8
Melany Baldeon <tochio12910@gmail.com>
2022-11-22 20:48:55 -0500

    added some javascript

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,15 @@
 var apiKey = "08f00304851866898e77f5deff0d7eba";
-var apiUrl = "https://api.themoviedb.org/3"
-var apiSearch = apiUrl + "/search/movie"
-var testQuery = "Bourne";
+var history = localStorage.getItem("searchHistory") ? JSON.parse(localStorage.getItem("searchHistory")) : [];
+var apiUrl = "https://api.themoviedb.org/3";
+var apiSearch = apiUrl + "/search/movie";
+var testQuery = "";
+var buttonEl = document.getElementById("button-enter");
 
-fetch(apiSearch + "?api_key=" + apiKey + "&query=" + testQuery)
+buttonEl.addEventListener("click", getTitle);
+
+function getTitle() { 
+    var inputBoxEl = document.getElementById("input-box").value;
+    testQuery = inputBoxEl;
+    console.log(testQuery)
+    fetch(apiSearch + "?api_key=" + apiKey + "&query=" + testQuery)
+}
